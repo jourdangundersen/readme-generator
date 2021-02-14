9363a75bb916e5a67148382f00ccea332684eb44
Jourdan Gundersen <jourdancgundersen@gmail.com>
2021-02-13 23:28:34 -0600

    added the prompt to ask the user all items within the questions array

--- index.js
+++ index.js
@@ -32,3 +32,85 @@ const licenseArray = [
 ];
 
 
+var fileContent;
+
+
+inquirer
+    .prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: questions[0],
+        },
+        {
+            name: 'github',
+            type: 'input',
+            message: questions[1],
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: questions[2],
+        },
+        {
+            name: 'title',
+            type: 'input',
+            message: questions[3],
+        },
+        {
+            name: 'description',
+            type: 'input',
+            message: questions[4],
+        },
+        {
+            name: 'license',
+            type: 'list',
+            message: questions[5],
+            choices: licenseArray,
+            default: 'none'
+        },
+        {
+            name: 'repo',
+            type: 'input',
+            message: questions[6],
+        },
+        {
+            name: 'install',
+            type: 'input',
+            message: questions[7],
+        },
+        {
+            name: 'usage',
+            type: 'input',
+            message: questions[8],
+        },
+        {
+            name: 'contribute',
+            type: 'input',
+            message: questions[9],
+        },
+        {
+            name: 'testing',
+            type: 'input',
+            message: questions[10],
+        },
+        {
+            name: 'url',
+            type: 'input',
+            message: questions[11],
+        },
+    ])
+
+    .then((data) => {
+        fileContent = generateMarkdown(data);
+        writeToFile('README.md', fileContent)
+    });
+
+
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('README Generated!'));
+
+}
+
+
